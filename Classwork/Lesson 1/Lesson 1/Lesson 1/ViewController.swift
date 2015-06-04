c1b4c4729cf5ff153cc67149af72466fbafacbec
Luke Kanter <lucaskanter92@gmail.com>
2015-06-03 21:18:52 -0400

    My first app WITH BUTTON!

---
+++ Classwork/Lesson 1/Lesson 1/Lesson 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson 1
+//
+//  Created by Luke Kanter on 6/3/15.
+//  Copyright (c) 2015 Luke Kanter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
