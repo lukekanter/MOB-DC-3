c8cce969a2ef6259e1f264de1ebec12f9c17cfa4
Luke Kanter <lucaskanter92@gmail.com>
2015-06-15 20:54:45 -0400

    6/14 classwork

---
+++ Classwork/Week 2/Lesson04-functionexample.playground/Contents.swift
@@ -0,0 +1,36 @@
+//import UIKit
+//// Function takes no parameter and returns no values
+//func mySimpleFunction() {
+//    println("Hey, I'm inside my simple function!")
+//}
+//mySimpleFunction()
+//
+//func aMoreComplexFunction(number: Int) {
+//    
+//    for i in 1...number {
+//        println(i)
+//    }
+//    println("Stop")
+//}
+//aMoreComplexFunction(42)
+//
+//func hare(age: Int, name: String) {
+//    println("My name is \(name) and I am \(age) years old")
+//}
+//
+//hare(23, "Luke")
+//
+//func functionWithOptional(age: Int, name: String, location: String?) {
+//    println("Hi, my name is \(name) and I am \(age) years old")
+//    if let location = location {
+//        println("I'm from \(location)")
+//    }
+//}
+//functionWithOptional(23, "Luke", "Virginia Beach, VA")
+//
+//func cat(name: String) -> String {
+//    let sentence = "My cat is named \(name)"
+//    return sentence
+//}
+//let myCat = cat("Davinci")
+//println(myCat)
