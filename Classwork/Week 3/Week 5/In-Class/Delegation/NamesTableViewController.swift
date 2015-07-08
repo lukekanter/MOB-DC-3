f3cdb4c7d4edcac5cd710efc9959543f94750b82
Luke Kanter <lucaskanter92@gmail.com>
2015-07-08 18:35:28 -0400

    prepare to pull

---
+++ Classwork/Week 3/Week 5/In-Class/Delegation/NamesTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NamesTableViewController.swift
+//  Delegation
+//
+//  Created by Thomas Degry on 6/29/15.
+//  Copyright (c) 2015 Thomas Degry. All rights reserved.
+//
+
+import UIKit
+
+class NamesTableViewController: UITableViewController, AddStudentDelegate {
+    
+    // Keep track of all names in this array
+    var names = [String]()
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("namecell", forIndexPath: indexPath) as! UITableViewCell
+        
+        // Fill out the user textLabel of the row by grabbing the student's name
+        let studentName = names[indexPath.row]
+        cell.textLabel?.text = studentName
+
+        return cell
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showAddStudents" {
+            let destinationVC = segue.destinationViewController as! AddViewController
+            destinationVC.delegate = self
+        }
+    }
+    
+    func addStudent(name: String) {
+        names.append(name)
+        tableView.reloadData()
+    }
+
+}
