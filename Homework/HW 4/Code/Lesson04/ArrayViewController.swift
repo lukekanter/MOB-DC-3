933a8fa0e4fe105d8f8090207c84bc1f7e62385e
Luke Kanter <lucaskanter92@gmail.com>
2015-07-12 16:56:10 -0400

    commented out my code for good manners

--- Homework/HW 4/Code/Lesson04/ArrayViewController.swift
+++ Homework/HW 4/Code/Lesson04/ArrayViewController.swift
@@ -9,20 +9,23 @@
 import UIKit
 
 class ArrayViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate {
-
+// hooking up the IB
     @IBOutlet weak var field: UITextField!
     @IBOutlet weak var tableView: UITableView!
+// declares an array to store entered values
     var array = [String]()
+// declares a cell ID to use to create a table view later
     let cellID = "cellID"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+// sets delegates and registers the class
         field.delegate = self
         tableView.delegate = self
         tableView.dataSource = self
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellID)
         
-        
+// I'm not sure why we added this in class
 //        self.dismissViewControllerAnimated(false, completion: nil)
         self.dismissViewControllerAnimated(false, completion: { () -> Void in
             self.view.backgroundColor = UIColor.blueColor()
@@ -30,14 +33,15 @@ class ArrayViewController: UIViewController, UITextFieldDelegate, UITableViewDat
         self.view.backgroundColor = UIColor.redColor()
         
     }
-    
+// enables return for the text field
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         self.view.endEditing(true)
+        // appends entered value to the array
         array.append(self.field.text)
         tableView.reloadData()
         return true
     }
-    
+// sets up functions to align with UITableView protocols
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return array.count
     }
--- Homework/HW 4/Code/Lesson04/MapViewController.swift
+++ Homework/HW 4/Code/Lesson04/MapViewController.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+// We need the Foundation kit to enable Notifications
 import Foundation
 
 class MapViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
-
+// hooking up the IB
     @IBOutlet weak var keyField: UITextField!
     @IBOutlet weak var valueField: UITextField!
     @IBOutlet weak var tableView: UITableView!
+// declaring dictionary, array of keys, and cell ID
     var dictionary = [String: String]()
     var keys = [String]()
     let cellID = "cellID"
-    
+// add observers in viewWillAppear to call functions later on
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "changeToBlue", name: UIKeyboardWillShowNotification, object: nil)
@@ -26,47 +28,52 @@ class MapViewController: UIViewController, UITextFieldDelegate, UITableViewDeleg
     
     override func viewDidLoad() {
         super.viewDidLoad()
+// declare delegates for variables, datasource for table view, and register the class using previously created cell ID
         keyField.delegate = self
         valueField.delegate = self
         tableView.delegate = self
         tableView.dataSource = self
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
-    
+// function listens for keyboard show notification to turn background blue
     func changeToBlue() {
         keyField.backgroundColor = UIColor.blueColor()
         valueField.backgroundColor = UIColor.blueColor()
     }
-    
+// function listens for keyboard hide notification to turn background red
     func changeToRed() {
         keyField.backgroundColor = UIColor.redColor()
         valueField.backgroundColor = UIColor.redColor()
     }
-    
+// UITextFieldDelegate function enables return function and details what to do with entered values
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         if textField == valueField {
             self.view.endEditing(true)
+            // adds text field values to a dictionary
             dictionary[keyField.text] = valueField.text
+            // appends key value to an array so it can be called later
             keys.append(keyField.text)
             tableView.reloadData()
             return true
         } else {
+            // directions say only the value field should be return enabled
             return false
         }
     }
-    
+// declaring functions that are part of table view protocol
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dictionary.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cellID") as! UITableViewCell
+        // optional binds value for key
         if let myKey = dictionary[keys[indexPath.row]] {
             cell.textLabel?.text = "\(keys[indexPath.row]) \(myKey)"
         }
         return cell
     }
-    
+// removing observers so as not to crash the app
     override func viewDidDisappear(animated: Bool) {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
